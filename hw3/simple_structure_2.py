c6e95e72f20045bfc6ac21afe45139191117cc9b
Louise Lai <louiselai88@gmail.com>
2017-09-25 09:43:36 -0400

    chuking works! Now for the grandfinale

--- hw3/simple_structure_2.py
+++ hw3/simple_structure_2.py
@@ -1,17 +1,20 @@
 import nltk
 simple_grammar_2 = nltk.CFG.fromstring("""
-	
+	S -> NP VP
 	NP -> NP PP
-	NP -> DT JJ NNS PP
-	PP -> IN DT NN
+	NP -> DT JJ NNS PP | DT NN NN
+	PP -> IN DT NN | VBP NP
 	DT -> 'any' | 'the'
 	JJ -> 'habitable'
 	NNS -> 'areas'
 	IN -> 'on'
-	NN -> 'planet'
+	NN -> 'planet' | 'border' | 'region'
+
+	VP -> VBP PP
+	VBP -> 'are' | 'in'
 	""")
 
-simple_sentence_2 = ['any', 'habitable', 'areas', 'on','the','planet']
+simple_sentence_2 = ['any', 'habitable', 'areas', 'on','the','planet','are', 'in', 'the', 'border', 'region']
 parser = nltk.ChartParser(simple_grammar_2)
 for tree in parser.parse(simple_sentence_2):
 	print(tree)
