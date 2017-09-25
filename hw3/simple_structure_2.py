0750adf4b42fb0d73746fe56cc5da73c8ac6a4b8
Louise Lai <louiselai88@gmail.com>
2017-09-25 09:34:22 -0400

    works for left structure

--- hw3/simple_structure_2.py
+++ hw3/simple_structure_2.py
@@ -2,11 +2,12 @@ import nltk
 simple_grammar_2 = nltk.CFG.fromstring("""
 	NP -> NP PP
 	NP -> DT JJ NNS PP
-	PP -> IN 'the' 'planet'
-	DT -> 'any'
+	PP -> IN DT NN
+	DT -> 'any' | 'the'
 	JJ -> 'habitable'
 	NNS -> 'areas'
 	IN -> 'on'
+	NN -> 'planet'
 	""")
 
 simple_sentence_2 = ['any', 'habitable', 'areas', 'on','the','planet']
